f3ab396e4539fe20e9b2e5dabcbfdd97adb904b3
CRONOSS <122462572+CRONOSS-Solaris@users.noreply.github.com>
2023-04-11 23:46:00 +0200

    Adding a configuration file for the "Miner" rank and adding an editing option through the torch GUI

--- RPGPlugin/Torch GUI/RPGPluginControl.xaml.cs
+++ RPGPlugin/Torch GUI/RPGPluginControl.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace RPGPlugin
 {
     public partial class RolesControl : UserControl
     {
-
         private Roles Plugin { get; }
 
         private RolesControl()
@@ -16,12 +17,23 @@ namespace RPGPlugin
         public RolesControl(Roles plugin) : this()
         {
             Plugin = plugin;
-            DataContext = plugin.Config;
+            DataContext = plugin;
         }
 
         public void Donate_Click(object sender, RoutedEventArgs e)
         {
             System.Diagnostics.Process.Start("https://www.paypal.com/donate/?hosted_button_id=HCV9695KQDMFN");
+        }
+
+        private void ExpRatioDataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                var mineral = ((KeyValuePair<string, double>)e.Row.Item).Key;
+                var newValue = double.Parse(((TextBox)e.EditingElement).Text);
+                Plugin.MinerConfig.ExpRatio[mineral] = newValue;
+                MinerConfig.SaveMinerConfig(Plugin.MinerConfig, Plugin.StoragePath);
+            }
         }
 
     }
