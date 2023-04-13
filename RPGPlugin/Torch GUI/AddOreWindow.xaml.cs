005de67b68ce034bb0c510f98d411ab4e5371005
CRONOSS <122462572+CRONOSS-Solaris@users.noreply.github.com>
2023-04-13 15:44:02 +0200

    Changed the type of the ExpPerOre property from int to double to allow floating point values to be entered. Code has been added to the AddButton_Click method to check if the value entered in the ExpPerOreTextBox field is a floating point number. If the value is not a number, an error message is displayed and the window will not close. In the SetExpPerOre method, the argument type has been changed from int to double to allow a floating point value to be set in the ExpPerOreTextBox field.

--- RPGPlugin/Torch GUI/AddOreWindow.xaml.cs
+++ RPGPlugin/Torch GUI/AddOreWindow.xaml.cs
@@ -15,9 +15,17 @@ namespace RPGPlugin
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             OreName = OreNameTextBox.Text;
-            ExpPerOre = double.Parse(ExpPerOreTextBox.Text);
-            DialogResult = true;
-            Close();
+
+            if (double.TryParse(ExpPerOreTextBox.Text, out double expPerOre))
+            {
+                ExpPerOre = expPerOre;
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Exp per ore must be a number.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         public void SetOreName(string oreName)
