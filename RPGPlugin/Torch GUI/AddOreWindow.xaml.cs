aa83881977a023d501c52c928b212b42eea3e5e0
CRONOSS <122462572+CRONOSS-Solaris@users.noreply.github.com>
2023-04-13 15:18:07 +0200

    1 A function for adding new ores has been added, allowing the user to define the name of an ore and the experience for mining it. 2 The ability to edit existing ores, including changing the experience value assigned to each ore, has been implemented. 3. the function of deleting selected ores from the configuration list has been implemented.

---
+++ RPGPlugin/Torch GUI/AddOreWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace RPGPlugin
+{
+    public partial class AddOreWindow : Window
+    {
+        public string OreName { get; set; }
+        public double ExpPerOre { get; set; }
+
+        public AddOreWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            OreName = OreNameTextBox.Text;
+            ExpPerOre = double.Parse(ExpPerOreTextBox.Text);
+            DialogResult = true;
+            Close();
+        }
+
+        public void SetOreName(string oreName)
+        {
+            OreNameTextBox.Text = oreName;
+        }
+
+        public void SetExpPerOre(double expPerOre)
+        {
+            ExpPerOreTextBox.Text = expPerOre.ToString();
+        }
+
+    }
+}
