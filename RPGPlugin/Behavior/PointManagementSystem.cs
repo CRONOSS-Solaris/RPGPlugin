0176485bbb2972e559444695cd9c7e56a32c7a5d
CRONOSS <122462572+CRONOSS-Solaris@users.noreply.github.com>
2023-04-14 13:49:22 +0200

    update

--- RPGPlugin/Behavior/PointManagementSystem.cs
+++ RPGPlugin/Behavior/PointManagementSystem.cs
@@ -49,13 +49,53 @@ namespace RPGPlugin.PointManagementSystem
 
             _queueInProcess = false;
         }
-        
+
         // WARRIOR SECTION
 
-        
-        
-        
-        
+        //// Method to add experience for a hunter for killing another player
+        //public async Task AddWarriorExpForKill(long warriorID)
+        //{
+        //    // Checking if the player data is available
+        //    If(!Roles.PlayerManagers.ContainsKey(warriorID)) return;
+
+        //    // Checking if the player's role is a hunter
+        //    If(Roles.PlayerManagers[warriorID].GetRole() != PlayerManager.FromRoles.Warrior) return;
+
+        //    // Retrieve the experience value per kill from the configuration
+        //    double expPerKill = WarriorConfig.LoadWarriorConfig().ExpPerKill;
+
+        //    // Adding experience for the hunter
+        //    await Roles.PlayerManagers[warriorID].AddWarriorExp(expPerKill);
+        //}
+
+        //// Method that adds experience for the hunter for destroying an enemy player's structure block
+        //Public async Task AddWarriorExpForDestroyedBlock(long warriorID, string blockType)
+        //{
+        //    // Checking if the player data is available
+        //    If(!Roles.PlayerManagers.ContainsKey(warriorID)) return;
+
+        //    // Checking if the player's role is a hunter
+        //    If(Roles.PlayerManagers[warriorID].GetRole() != PlayerManager.FromRoles.Warrior) return;
+
+        //    // Retrieving the experience value for destroying a block from the configuration
+        //    double expPerDestroyedBlock = WarriorConfig.LoadWarriorConfig().ExpRatio[blockType];
+
+        //    // Adding experience for the hunter
+        //    await Roles.PlayerManagers[warriorID].AddWarriorExp(expPerDestroyedBlock);
+        //}
+
+//Add an event or modify an existing event that is triggered when a player kills another player. 
+
+//Then call AddWarriorExpForKill with the player ID as an argument.
+
+//Add an event or modify an existing event that is triggered when a player destroys an enemy player's structure block. Then call 
+
+//AddWarriorExpForDestroyedBlock with the player ID and the type of block destroyed as arguments.
+
+//Make sure you load the hunter's configuration (WarriorConfig) similar to the miner's configuration (MinerConfig). You can do this by adding
+
+//WarriorConfig.LoadWarriorConfig() in the appropriate place, just as you did for MinerConfig.LoadMinerConfig().
+
         public void Dispose()
         {
             _QueueTimer?.Dispose();
--- RPGPlugin/Config/WarriorConfig.cs
+++ RPGPlugin/Config/WarriorConfig.cs
@@ -7,7 +7,23 @@
 
 //namespace RPGPlugin
 //{
-//    public class WarriorConfig : ViewModel 
+//    public class WarriorConfig : ViewModel
 //    {
+//        public Dictionary<string, double> ExpRatio { get; set; }
+//        public double ExpPerKill { get; set; }
+//        public double ExpPerDestroyedBlock { get; set; }
+
+//        public WarriorConfig()
+//        {
+//            ExpRatio = new Dictionary<string, double>
+//            {
+//              { "Player", 100.0 }, // Example of experience value for killing a player
+//              { "SmallBlock", 2.0 }, // Example of experience value for destroying a small block
+//              { "LargeBlock", 10.0 } // Example of experience value for destroying a large block
+//            };
+
+//            ExpPerKill = 100.0;
+//            ExpPerDestroyedBlock = 10.0;
+//        }
 //    }
 //}
